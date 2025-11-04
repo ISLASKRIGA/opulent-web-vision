bacca4755befdba08482a13d9050402cbcf43348
DeadIvan360 <ivan.estrada.enp5.123@comunidad.unam.mx>
2025-11-03 23:11:04 -0600

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Clients from "@/components/Clients";
+import { Timeline } from "@/components/Timeline";
+import { timelineData } from "@/components/timelineData";
 import Pricing from "@/components/Pricing";
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Footer from "@/componentsV/Footer";
 
 const Index = () => {
   return (
@@ -12,6 +14,7 @@ const Index = () => {
       <main>
         <Hero />
         <Clients />
+        <Timeline data={timelineData} />
         <Pricing />
         <Contact />
       </main>
